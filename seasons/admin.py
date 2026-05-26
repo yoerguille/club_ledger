c56da8cc362f10f07211399535edce6e805c99c4
Guille <yoerguille@gmail.com>
2026-05-27 00:27:49 +0200

    feat(seasons): add season model and admin

--- seasons/admin.py
+++ seasons/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Season
 # Register your models here.
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "start_date",
+        "end_date",
+        "is_active",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+    )
--- seasons/models.py
+++ seasons/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Season(models.Model):
+    name = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name="Temporada",       
+    )
+
+    start_date = models.DateField(
+        verbose_name="Fecha de inicio",
+    )
+
+    end_date = models.DateField(
+        verbose_name="Fecha fin",
+    )
+
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name="Activa",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Temporada'
+        verbose_name_plural = 'Temporadas'
+        ordering = ["-start_date"]
+
+    def __str__(self):
+        return self.name
