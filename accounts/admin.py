ea5ef5e0ff60e046f69d69640b3ee741265cc7bd
Guille <yoerguille@gmail.com>
2026-05-27 00:43:49 +0200

    feat(accounts): add account model and admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Account
 # Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "customer",
+        "season",
+        "created_at",
+    )
+
+    search_fields = (
+        "customer__name",
+    )
+
+    list_filter = (
+        "season"
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from customers.models import Customer
+from seasons.models import Season
 
 # Create your models here.
+
+class Account(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.PROTECT,
+        related_name="accounts",
+        verbose_name="Cliente",
+    )
+
+    season = models.ForeignKey(
+        Season,
+        on_delete=models.PROTECT,
+        related_name="accounts",
+        verbose_name="Temporada",
+    )
+
+    notes = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Noats",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Cuenta'
+        verbose_name_plural = 'Cuentas'
+        ordering = ["-created_at"]
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["customer", "season"],
+                name="unique_customer_season_account"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.customer.name} - {self.season.name}"
+    
