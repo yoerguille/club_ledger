0bfaae759a34911523cbd9c322a6078200e524a3
Guille <yoerguille@gmail.com>
2026-05-27 00:12:28 +0200

    feat(customers): add customer model and admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cif",
+        "email",
+        "phone",
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+        "cif",
+        "email",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
