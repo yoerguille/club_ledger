a360ad4d0adc382ecdf2dc18d2b7a8f840a59646
Guille <yoerguille@gmail.com>
2026-05-28 17:58:04 +0200

    feat: add customer list view

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Customer
 
 # Create your views here.
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "customers/customers_list.html"
+    context_object_name = "customers"
