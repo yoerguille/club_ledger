de202d6175c0cd6ddf572aa2d05ec3ce94603eb7
Guille <yoerguille@gmail.com>
2026-06-01 16:44:20 +0200

    little fix

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("customers/", include("customers.urls")),
+    path("customers/", include("customers.urls", namespace='customers')),
 ]
--- customers/urls.py
+++ customers/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import CustomerListView
 
+app_name = 'customers'
+
 urlpatterns = [
     path("", CustomerListView.as_view(), name="customer_list"),
     
