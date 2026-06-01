4fa01f026b8d8ed56710669a5c810c2890b3bd39
Guille <yoerguille@gmail.com>
2026-06-01 17:15:23 +0200

    feat(customers): add customer list view

--- customers/urls.py
+++ customers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CustomerListView
+from .views import CustomerListView, CustomerDetailView
 
 app_name = 'customers'
 
 urlpatterns = [
     path("", CustomerListView.as_view(), name="customer_list"),
+    path("<int:pk>/", CustomerDetailView.as_view(), name="customer_detail"),
     
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Customer
 
 # Create your views here.
@@ -7,3 +7,8 @@ class CustomerListView(ListView):
     model = Customer
     template_name = "customers/customers_list.html"
     context_object_name = "customers"
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name ="customers/customers_detail.html"
+    context_object_name = 'customer'
