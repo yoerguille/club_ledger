051b9f059157c9d886bcbfab274d68e7773462e2
Guille <yoerguille@gmail.com>
2026-09-14 19:05:07 +0200

    feature:dropdown and forms styles

--- apps/customers/forms.py
+++ apps/customers/forms.py
@@ -14,3 +14,14 @@ class CustomerForm(forms.ModelForm):
             'adress',
             'contact_person',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        base_classes = (
+            "w-full rounded-lg border border-gray-300 bg-white/5 "
+            "px-3 py-2.5 text-sm text-gray-100 placeholder-gray-500 "
+            "focus:border-blue-500 focus:outline-none focus:ring-2 "
+            "focus:ring-blue-500/20"
+        )
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": base_classes})
